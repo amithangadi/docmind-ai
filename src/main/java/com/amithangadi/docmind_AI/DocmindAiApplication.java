0fc3b04fe4dc90a9a318016ccd4c6e9619321b11
amithangadi <amithangadi1234@gmail.com>
2026-08-01 13:51:42 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/amithangadi/docmind_AI/DocmindAiApplication.java
@@ -0,0 +1,13 @@
+package com.amithangadi.docmind_AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocmindAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocmindAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amithangadi/docmind_AI/DocmindAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amithangadi.docmind_AI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocmindAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
